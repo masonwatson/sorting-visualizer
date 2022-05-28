6ce60fd955d636f04ec8672791a0a73e2fb98d6e
masonwatson <84687363+masonwatson@users.noreply.github.com>
2022-05-27 19:38:24 -0500

    changed aesthetic of site and array

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import * as ms from './algorithms/mergeSort';
-import * as qs from './algorithms/quickSort';
-import * as bs from './algorithms/bubbleSort';
+import * as ms from "./algorithms/mergeSort";
+import * as qs from "./algorithms/quickSort";
+import * as bs from "./algorithms/bubbleSort";
 import "./SortingVisualizer.css";
 
 const MERGE_SORT_ANIMATION_SPEED_MS = 2;
 const QUICK_SORT_ANIMATION_SPEED_MS = 5;
 const BUBBLE_SORT_ANIMATION_SPEED_MS = 0.3;
-const NUMBER_OFARRAY_BARS = 150; 
+const NUMBER_OFARRAY_BARS = 120;
 
 export default function SortingVisualizer() {
   const [array, setArray] = useState([]);
@@ -25,7 +25,7 @@ export default function SortingVisualizer() {
     const animations = ms.mergeSort(array);
 
     for (let i = 0; i < animations.length; i++) {
-      const arrayBars = document.getElementsByClassName('array-bar');
+      const arrayBars = document.getElementsByClassName("array-bar");
 
       // Every third animation will be false.
       const isColorChange = i % 3 !== 2;
@@ -35,9 +35,9 @@ export default function SortingVisualizer() {
         const barOneStyle = arrayBars[barOneIdx].style;
         const barTwoStyle = arrayBars[barTwoIdx].style;
 
-        // Every first color change will be to red. 
+        // Every first color change will be to red.
         // Every second color change will be back to white.
-        const color = i % 3 === 0 ? 'red' : 'white';
+        const color = i % 3 === 0 ? "red" : "white";
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
           barTwoStyle.backgroundColor = color;
@@ -56,7 +56,7 @@ export default function SortingVisualizer() {
     const animations = qs.quickSort(array);
 
     for (let i = 0; i < animations.length; i++) {
-      const arrayBars = document.getElementsByClassName('array-bar');
+      const arrayBars = document.getElementsByClassName("array-bar");
 
       setTimeout(() => {
         const [barOneIdx, newHeight] = animations[i];
@@ -66,27 +66,26 @@ export default function SortingVisualizer() {
     }
   }
 
-  function runHeapSort() {}
+  // function runHeapSort() {}
 
   function runBubbleSort() {
     const animations = bs.bubbleSort(array);
 
     for (let i = 0; i < animations.length; i++) {
-      const arrayBars = document.getElementsByClassName('array-bar');
+      const arrayBars = document.getElementsByClassName("array-bar");
 
-      // Every third and fourth animation will be false. 
+      // Every third and fourth animation will be false.
       let isColorChange = true;
-      if ((i % 4 === 2) || (i % 4 === 3))
-        isColorChange = false;
+      if (i % 4 === 2 || i % 4 === 3) isColorChange = false;
 
       if (isColorChange) {
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOneStyle = arrayBars[barOneIdx].style;
         const barTwoStyle = arrayBars[barTwoIdx].style;
 
-        // Every first color change will be to red. 
+        // Every first color change will be to red.
         // Every second color change will be back to white.
-        const color = i % 4 === 0 ? 'red' : 'white';
+        const color = i % 4 === 0 ? "red" : "white";
 
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
@@ -103,24 +102,55 @@ export default function SortingVisualizer() {
   }
 
   return (
-    <div className="vh-100 d-flex justify-content-center align-items-center" draggable="false">
+    <div
+      className="vh-100 d-flex justify-content-center align-items-center"
+      draggable="false"
+    >
       <div className="sorting-visualizer-wrapper">
-        <div className="array-container">
-          {array.map((value, idx) => {
-            return (
-              <div
-                className="array-bar"
-                key={idx}
-                style={{ height: `${value}px` }}
-              />
-            );
-          })}
+        <div className="array-container d-flex align-items-start flex-column">
+          {/* <div className="array postion-absolute absolute-bottom w-100"> */}
+          <div className="array mb-auto">
+            {array.map((value, idx) => {
+              return (
+                <div
+                  className="array-bar"
+                  key={idx}
+                  style={{ height: `${value}px` }}
+                />
+              );
+            })}
+          </div>
         </div>
-        <button onClick={() => resetArray()}>Generate New Array</button>
-        <button onClick={() => runMergeSort()}>Merge Sort</button>
-        <button onClick={() => runQuickSort()}>Quick Sort</button>
-        {/* <button onClick={() => runHeapSort()}>Heap Sort</button> */}
-        <button onClick={() => runBubbleSort()}>Bubble Sort</button>
+        <button
+          type="button"
+          className="btn btn-clear inline-block-content"
+          onClick={() => resetArray()}
+        >
+          Generate New Array
+        </button>
+        <button
+          type="button"
+          className="btn btn-clear inline-block-content"
+          onClick={() => runMergeSort()}
+        >
+          Merge Sort
+        </button>
+        <button
+          type="button"
+          className="btn btn-clear inline-block-content"
+          onClick={() => runQuickSort()}
+        >
+          Quick Sort
+        </button>
+        {/* <button type="button"
+            className="btn btn-clear inline-block-content" onClick={() => runHeapSort()}>Heap Sort</button> */}
+        <button
+          type="button"
+          className="btn btn-clear inline-block-content"
+          onClick={() => runBubbleSort()}
+        >
+          Bubble Sort
+        </button>
       </div>
     </div>
   );
